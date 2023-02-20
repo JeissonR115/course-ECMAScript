a821c17554a417db375fdf029b9f4249566ee846
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-20 11:50:22 -0500

    es6-arrow-functions

---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,12 @@
+// funcion normal
+const square = function (num) {
+    return num*num
+}
+
+// arrow function
+const square = (num) => {
+    return num*num
+}
+
+const square = num => num*num
+//mas info: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Functions/Arrow_functions
