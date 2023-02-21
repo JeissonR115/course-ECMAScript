e8d163c36e1f3e27c0a36d40eef1cb397f4a5b9d
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-20 22:57:56 -0500

    es6-rest-speard

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,9 @@
+//array destructuring
+let fruits = ['banana','appel']
+let [b,a] = fruits
+console.log(b,fruits[0]);
+
+//object destructuring
+let user = {userName: 'jeissonr115', age: 17}
+let {userName,age} = user
+console.log(userName,age);
