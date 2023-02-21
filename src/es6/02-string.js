40617ffd0884b989ea40178b4f5c1c1fcce548b6
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-20 22:35:49 -0500

    es6-strings

---
+++ src/es6/02-string.js
@@ -0,0 +1,15 @@
+let hello = 'hello'
+let world = 'world'
+let epicPharase = hello + ' ' + world+ '!'
+console.log(epicPharase);
+
+// Templeit literals
+let epicPharase2 = `${hello} ${world}!`
+console.log(epicPharase2);
+//multi line strings
+console.log('   *  *\n'+'   *  *  \n'+' *      *\n'+'  ****** \n')
+console.log(`
+   *  *
+   *  *  
+ *      *
+  ****** `);
