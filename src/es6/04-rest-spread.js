33dd928e7cf4288ccca4a360979ffe995acbb070
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-21 09:53:16 -0500

    es6-rest-spread-2

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -7,3 +7,20 @@ console.log(b,fruits[0]);
 let user = {userName: 'jeissonr115', age: 17}
 let {userName,age} = user
 console.log(userName,age);
+
+//sread operator
+
+let person = {name: 'ana',age:'40'}
+let country = 'MX'
+
+let data = {id: 1,...person,country}
+console.log(data);
+
+//rest
+
+function sum(num, ...values) {
+    console.log(values)
+    console.log(num+values[0])
+    return num+values[0]
+}
+sum(1,2,9,65,32,5,6,6)
