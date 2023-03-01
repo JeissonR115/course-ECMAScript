257a05cbed52db5941901df25bba6f60d6bbdb9b
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-28 22:20:23 -0500

    es8-Object-values

---
+++ src/es8/01-object-values.js
@@ -0,0 +1,8 @@
+const countries = {
+    MX:'Mexico',
+    CO:'Colombia',
+    CH:'Chile',
+    PE:'Peru'
+}
+console.log(Object.values(countries));//crea un array con los valores del objeto
+// [ 'Mexico', 'Colombia', 'Chile', 'Peru' ]
