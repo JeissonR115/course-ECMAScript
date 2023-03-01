e6f92fd7d8d682d26ff1afc84499b380fcc18471
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-28 22:08:34 -0500

    es7-array-includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers = [0,1,2,3,4];
+console.log(`el Array numbers incluye un cuatro?\n${numbers.includes(4)}\n`)//true
+console.log(`el Array numbers incluye un cinco?\n${numbers.includes(5)}\n`)//false
+
+let list = ['a','b','c','d']
+console.log(`el Array list incluye la letra "a"?\n${list.includes('a')}`)//true
+console.log(`el Array list incluye la letra "A"?\n${list.includes('A')}`)//false
