8689257b0b06cafc4b4ab1614a14310d64df29ea
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-28 22:16:51 -0500

    es8-Object-entries

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,13 @@
+const countries = {
+    MX:'Mexico',
+    CO:'Colombia',
+    CH:'Chile',
+    PE:'Peru'
+}
+console.log(Object.entries(countries));// tranforma objetos en arrats
+/*[
+    [ 'MX', 'Mexico' ],
+    [ 'CO', 'Colombia' ],
+    [ 'CH', 'Chile' ],
+    [ 'PE', 'Peru' ]
+  ]*/
