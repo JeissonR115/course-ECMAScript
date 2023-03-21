bfadfd202412dc88d3cc29a626439f940eca0d94
jeissoR115 <jeissonfeliper@gmail.com>
2023-03-20 22:53:07 -0500

    es8-async-function

--- src/es8/02-string-padding.js
+++ src/es8/02-string-padding.js
@@ -1,4 +1,5 @@
 const string = 'string'
-console.log(string.padStart(12,'.'));//rellenar el String al inicio con "." hasta que el tenga 12 caracteres
-console.log(string.padEnd(12,'.'));//rellenar el String al final con "." hasta que el tenga 12 caracteres
-console.log(string.padStart(12,'.').padEnd(18,'.'));
+const size = 20;
+console.log(string.padStart(size,'.'));//rellenar el String al inicio con "." hasta que el tenga size caracteres
+console.log(string.padEnd(size,'.'));//rellenar el String al final con "." hasta que el tenga size caracteres
+console.log(string.padStart(size - ( size - string.length)/2  ,'.').padEnd(size,'.'));
